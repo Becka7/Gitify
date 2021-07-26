418046634f0974caef43564114ab8e1131441285
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-07-27 02:43:51 +0300

    craeted a get repos method

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -4,4 +4,5 @@ import { stringify } from '@angular/compiler/src/util';
 }
 }
   
+  let RepoUrl='https://api.github.com/users/'+username+'/repos?order=created&sort=asc?access_token='+environment.Apikey ;
   }
