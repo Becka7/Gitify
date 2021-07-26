afbf06f90b08b5dd54e71c862557c04d30ceeb99
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-07-27 02:45:40 +0300

    created class user

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  Apikey:"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"
 };
 
 /*
