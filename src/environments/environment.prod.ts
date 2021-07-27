d696efee84a4fcb594607e4cc8b595d5738f9ec4
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-07-27 11:47:47 +0300

    change apikey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
   production: true,
-  Apikey:"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",
+  Apikey:"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",
   
 };
 
-// https://api.github.com/users/Becka7?access_token=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
-
-// https://api.github.com/users/Becka7?access_token=%20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
